4d4788999ce8b049b645cf7f4f04d2e12903261a
SAI GANESH DOKALA <61933709+saiganesh-d@users.noreply.github.com>
2021-07-22 21:44:04 +0530

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-import os
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
